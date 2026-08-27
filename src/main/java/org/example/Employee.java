381edfca58b2d3e355014bf6baf89d17b957f2d2
Sabeeha <sabeehas545@gmail.com>
2026-08-27 11:05:35 -0500

    new file

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Employee {
+
+    private Integer empId;
+
+    private String empName;
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/EmployeeTestCase.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeTestCase {
+
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee();
+        emp1.setEmpId(101);
+        emp1.setEmpName("Arun");
+
+        Employee emp2 = new Employee();
+        emp2.setEmpId(102);
+        emp2.setEmpName("Suresh");
+
+        Employee emp3 = new Employee();
+        emp3.setEmpId(103);
+        emp3.setEmpName("Varun");
+
+        List<Employee> empList = new ArrayList<>();
+
+       empList.add(emp1);
+       empList.add(emp2);
+       empList.add(emp3);
+
+       System.out.println("List of Employees:");
+
+       for (Employee employee : empList){
+
+           System.out.println(employee);
+       }
+
+
+    }
+}
